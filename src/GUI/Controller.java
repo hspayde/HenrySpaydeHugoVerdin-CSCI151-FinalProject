40d052ddc1066bf4a47d121953f8f1c77c11cf23
hspayde <spaydeht@hendrix.edu>
2024-05-02 16:34:31 -0500

    Controller now starts the other GUI's when buttons pushed.

--- src/GUI/Controller.java
+++ src/GUI/Controller.java
@@ -2,11 +2,16 @@ package GUI;
 
 import Model.Book;
 import Model.Shelf;
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -24,9 +29,9 @@ public class Controller {
     @FXML
     private Pane pane3;
 
-    Book test = new Book("Author", "Title", 55, 25, true, true, true, true, true, 4, "comments");
-    Book test1 = new Book("Autho", "Tite", 5, 2, true, true, true, true, true, 4, "comments");
-    Book test2 = new Book("Auor", "tle", 85, 5, true, true, true, true, true, 4, "comments");
+    Book test = new Book("Author", "Title", 55, 25, true, true, true, true, true, "genre", 4, "comments");
+    Book test1 = new Book("Autho", "Tite", 5, 2, true, true, true, true, true, "genre", 4, "comments");
+    Book test2 = new Book("Auor", "tle", 85, 5, true, true, true, true, true, "genre", 4, "comments");
 
 
 
@@ -81,49 +86,49 @@ public class Controller {
         Book current;
 
         double bookWidth = pane1.getPrefWidth() /50;
-
-
-        for(int i = 0; i < displayed.size(); i++) {
-            double bookHeight = rand.nextDouble(50, 75);
-            if(i < 50) {
-                Rectangle book = new Rectangle(i * bookWidth, pane1.getPrefHeight() - bookHeight, bookWidth, bookHeight);
-                book.setFill(colors[rand.nextInt(0, colors.length - 1)]);
-                book.setStroke(Color.BLACK);
-                current = displayed.get(i);
-                Book finalCurrent = current;
-                book.setOnMouseEntered(event -> {
-                    mouseEntered(finalCurrent);
-                });
-                book.setOnMouseClicked(event -> {
-                    mouseClicked(finalCurrent);
-                });
-                pane1.getChildren().add(book);
-            }else if(i < 100) {
-                Rectangle book = new Rectangle(i * bookWidth, pane2.getPrefHeight() - bookHeight, bookWidth, bookHeight);
-                book.setFill(colors[rand.nextInt(0, colors.length - 1)]);
-                book.setStroke(Color.BLACK);
-                current = displayed.get(i);
-                Book finalCurrent = current;
-                book.setOnMouseEntered(event -> {
-                    mouseEntered(finalCurrent);
-                });
-                book.setOnMouseClicked(event -> {
-                    mouseClicked(finalCurrent);
-                });
-                pane2.getChildren().add(book);
-            }else if(i < 150) {
-                Rectangle book = new Rectangle(i * bookWidth, pane3.getPrefHeight() - bookHeight, bookWidth, bookHeight);
-                book.setFill(colors[rand.nextInt(0, colors.length - 1)]);
-                book.setStroke(Color.BLACK);
-                current = displayed.get(i);
-                Book finalCurrent = current;
-                book.setOnMouseEntered(event -> {
-                    mouseEntered(finalCurrent);
-                });
-                book.setOnMouseClicked(event -> {
-                    mouseClicked(finalCurrent);
-                });
-                pane3.getChildren().add(book);
+        if(displayed != null) {
+            for (int i = 0; i < displayed.size(); i++) {
+                double bookHeight = rand.nextDouble(50, 75);
+                if (i < 50) {
+                    Rectangle book = new Rectangle(i * bookWidth, pane1.getPrefHeight() - bookHeight, bookWidth, bookHeight);
+                    book.setFill(colors[rand.nextInt(0, colors.length - 1)]);
+                    book.setStroke(Color.BLACK);
+                    current = displayed.get(i);
+                    Book finalCurrent = current;
+                    book.setOnMouseEntered(event -> {
+                        mouseEntered(finalCurrent);
+                    });
+                    book.setOnMouseClicked(event -> {
+                        mouseClicked(finalCurrent);
+                    });
+                    pane1.getChildren().add(book);
+                } else if (i < 100) {
+                    Rectangle book = new Rectangle(i * bookWidth, pane2.getPrefHeight() - bookHeight, bookWidth, bookHeight);
+                    book.setFill(colors[rand.nextInt(0, colors.length - 1)]);
+                    book.setStroke(Color.BLACK);
+                    current = displayed.get(i);
+                    Book finalCurrent = current;
+                    book.setOnMouseEntered(event -> {
+                        mouseEntered(finalCurrent);
+                    });
+                    book.setOnMouseClicked(event -> {
+                        mouseClicked(finalCurrent);
+                    });
+                    pane2.getChildren().add(book);
+                } else if (i < 150) {
+                    Rectangle book = new Rectangle(i * bookWidth, pane3.getPrefHeight() - bookHeight, bookWidth, bookHeight);
+                    book.setFill(colors[rand.nextInt(0, colors.length - 1)]);
+                    book.setStroke(Color.BLACK);
+                    current = displayed.get(i);
+                    Book finalCurrent = current;
+                    book.setOnMouseEntered(event -> {
+                        mouseEntered(finalCurrent);
+                    });
+                    book.setOnMouseClicked(event -> {
+                        mouseClicked(finalCurrent);
+                    });
+                    pane3.getChildren().add(book);
+                }
             }
         }
     }
@@ -133,11 +138,33 @@ public class Controller {
     }
 
     public void mouseClicked(Book book) {
-
+        Stage stage = new Stage();
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            BorderPane root =
+                    (BorderPane)loader.load(getClass().getResource("BookInfo.fxml").openStream());
+            stage.setScene(new Scene(root));
+            stage.show();
+            root.requestFocus();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 
     public void addBook() {
-
+        Stage stage = new Stage();
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            BorderPane root =
+                    (BorderPane)loader.load(getClass().getResource("CreateBook.fxml").openStream());
+            stage.setScene(new Scene(root));
+            stage.show();
+            root.requestFocus();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 
     public ArrayList<Book> getList(Shelf current) {
